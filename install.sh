baf90a19f5b3a824e1f9c5cd5251c15e84b14d5c
Göran Thyni <gthyni@gmail.com>
2024-12-25 15:03:24 +0100

    new file install

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# installation file for docker on debian/ubuntu
+# Copyright (C) 2024
+# licence: GNU GPL v3
+
+# update the system
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt -y install docker
+
+
